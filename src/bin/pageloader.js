07d3a5c195901be86c5858356808822c8b63dcc8
sethlan <sethlans@protonmail.com>
2018-05-22 19:33:55 +0800

    try make download site

--- src/bin/pageloader.js
+++ src/bin/pageloader.js
@@ -7,16 +7,22 @@ import path from 'path';
 import version from '../../package.json';
 import pageload from '..';
 
+let pathDir;
+let addr;
 program
   .version(version)
   .usage('[options] <url> <whereToSafe>')
-  .arguments('<path> <url>')
+  .arguments('<pathForSave> <address>')
+  .action((pathForSave, address) => {
+    pathDir = pathForSave;
+    addr = address;
+  })
   .parse(process.argv);
-pageload(program.url)
+pageload(addr)
   .then((data) => {
     const { hostname, pathname } = url.parse(program.url);
     const filename = `${hostname.replace(/\//g, '-')}-${pathname.replace(/\//g, '-')}.html`;
-    const pathForSave = path.resolve(program.path, filename);
+    const pathForSave = path.resolve(pathDir, filename);
     return fs.writeFile(pathForSave, data);
   })
   .catch(error => console.log(error));
