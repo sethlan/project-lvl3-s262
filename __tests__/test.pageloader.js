a3330f9c5aa9611855d3c1f2032cbb5fbb24de4c
sethlan <sethlans@protonmail.com>
2018-05-22 15:48:38 +0800

    fix test 50/50

--- __tests__/test.pageloader.js
+++ __tests__/test.pageloader.js
@@ -1,14 +1,25 @@
 import nock from 'nock';
 import pageload from '../src/';
 
-const host = 'http://ru.hexlet.io';
+const host = 'http://localhost';
 const path = '/';
 const answer = { id: 'done' };
 nock(host)
   .get(path)
   .reply(200, answer);
-test('work for small chunk', () => {
+test('test for http://localhost', () => {
   expect.assertions(1);
   return pageload(host)
+    .then(data => expect(data).toEqual(answer));
+});
+
+const host2 = 'http://ru.hexlet.io';
+const path2 = '/';
+nock(host2)
+  .get(path2)
+  .reply(200, answer);
+test('test for http://ru.hexlet.io', () => {
+  expect.assertions(1);
+  return pageload(host2)
     .then(data => expect(data).toEqual(answer));
 });
--- src/index.js
+++ src/index.js
@@ -1,31 +1,31 @@
 import axios from 'axios';
 import httpAdapter from 'axios/lib/adapters/http';
-import http from 'http';
+// import http from 'http';
 
 export default url => new Promise((resolve, reject) => {
-  // axios.defaults.host = url;
-  // axios.defaults.adapter = httpAdapter;
-  // axios.get('/user')
-  //   .then((response) => {
-  //     resolve(response.data);
-  //   })
-  //   .catch((error) => {
-  //     console.log(error);
-  //     reject(error);
-  //   });
-  http.get(url, (res) => {
-    res.setEncoding('utf8');
-    let rawData = '';
-    res.on('data', (chunk) => { rawData += chunk; });
-    res.on('end', () => {
-      try {
-        const json = JSON.parse(rawData);
-        resolve(json);
-      } catch (e) {
-        reject(e.message);
-      }
+  axios.defaults.host = url;
+  axios.defaults.adapter = httpAdapter;
+  axios.get('/')
+    .then((response) => {
+      resolve(response.data);
+    })
+    .catch((error) => {
+      // console.log(error);
+      reject(error);
     });
-  }).on('error', (e) => {
-    reject(e.message);
-  });
+  // http.get(url, (res) => {
+  //   res.setEncoding('utf8');
+  //   let rawData = '';
+  //   res.on('data', (chunk) => { rawData += chunk; });
+  //   res.on('end', () => {
+  //     try {
+  //       const data = JSON.parse(rawData);
+  //       resolve(data);
+  //     } catch (e) {
+  //       reject(e.message);
+  //     }
+  //   });
+  // }).on('error', (e) => {
+  //   reject(e.message);
+  // });
 });
