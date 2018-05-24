65cf2d9e03d11262c649b74dc5612805c355560b
sethlan <sethlans@protonmail.com>
2018-05-24 23:08:28 +0800

    add img

--- __tests__/test.pageloader.js
+++ __tests__/test.pageloader.js
@@ -8,10 +8,16 @@ const dataForSave = 'test';
 const host = 'http://www.example.com';
 const pathName = '/';
 const folderForTest = path.join(os.tmpdir(), 'pageloader');
-test('test for http://localhost', async () => {
+test('test for hyper text only', async () => {
   nock(host).get(pathName).reply(200, dataForSave);
   const folder = await fs.mkdtemp(folderForTest);
   const pathToFile = await pageload(host + pathName, folder);
   const dataFromFunc = await fs.readFile(pathToFile, 'utf8');
   return expect(dataFromFunc).toBe(dataForSave);
 });
+
+// test('test for hyper text and image', async () => {
+//   const html = await fs.readFile('../test.html');
+//   nock(host).get(pathName).reply(200, html);
+//
+// })
