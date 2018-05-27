0b22bce04983bf2dacc693cdb15fd031ca8733b7
sethlan <sethlans@protonmail.com>
2018-05-28 02:17:14 +0800

    add debug

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import url from 'url';
 import path from 'path';
 import cheerio from 'cheerio';
 import httpAdapter from 'axios/lib/adapters/http';
+import debug from 'debug';
+
+const log = debug('page-loader');
 
 const namingFile = (addr) => {
   const { hostname, pathname } = url.parse(addr);
@@ -14,25 +17,28 @@ const namingFile = (addr) => {
 };
 
 export default (addr, pathDir) => {
+  log('start program');
   axios.defaults.adapter = httpAdapter;
   const filename = namingFile(addr);
   const pathForSave = path.resolve(pathDir, filename);
   const dirForRes = path.resolve(pathDir, `${filename}_files`);
   const resources = [];
-  let $;
+  let jquery;
   return axios.get(addr)
     .then(({ data }) => {
-      $ = cheerio.load(data);
-      return Promise.all(['img', 'link', 'script'].map(e => $(e).each((i, element) => {
-        const { hostname } = $(element).attr('src') ? url.parse($(element).attr('src')) : { hostname: 'not' };
+      log('downlad html');
+      jquery = cheerio.load(data);
+      return Promise.all(['img', 'link', 'script'].map(e => jquery(e).each((i, element) => {
+        const { hostname } = jquery(element).attr('src') ? url.parse(jquery(element).attr('src')) : { hostname: 'not' };
         if (!hostname) {
-          const name = $(element).attr('src').replace(/\//g, (str, offset, s) =>
+          const name = jquery(element).attr('src').replace(/\//g, (str, offset, s) =>
             (offset === (s.length - 1 || 0) ? '' : '-'));
           const config = {
-            url: url.resolve(addr, $(element).attr('src')),
+            url: url.resolve(addr, jquery(element).attr('src')),
             method: 'get',
             responseType: 'stream',
           };
+          log('make config');
           axios(config).then((img) => {
             const nameForRes = path.resolve(dirForRes, name);
             resources.push(nameForRes);
@@ -40,12 +46,14 @@ export default (addr, pathDir) => {
               fs.mkdir(dirForRes);
             }
             fs.writeFile(nameForRes);
-            $(element).attr('src', nameForRes);
+            jquery(element).attr('src', nameForRes);
             img.data.pipe(fs.createWriteStream(nameForRes));
+            log('save img', nameForRes);
           });
         }
       })));
-    })
-    .then(() => fs.writeFile(`${pathForSave}.html`, $.html()))
-    .then(() => [`${pathForSave}.html`, ...resources]);
+    }).then(() => {
+      log('write html');
+      return fs.writeFile(`${pathForSave}.html`, jquery.html());
+    }).then(() => [`${pathForSave}.html`, ...resources]);
 };
