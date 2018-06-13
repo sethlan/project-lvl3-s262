7adb3c71ea4f42cb346c4cd4be514774b569dcdd
sethlans <sethlans@protonmail.com>
2018-06-13 17:42:45 +0800

    rewrite download resources

--- src/index.js
+++ src/index.js
@@ -16,6 +16,24 @@ const namingFile = (addr) => {
   return `${normHost}${normPath}`;
 };
 
+const namingRes = attr => attr.replace(/\//g, (str, offset, s) => (offset === (s.length - 1 || 0) ? '' : '-'));
+
+const changeHtmlAndDownloadLocalRes = (html) => {
+  const jquery = cheerio.load(html);
+  const pathToRes = [];
+  jquery('img', 'link', 'script').each((i, element) => {
+    const attr = element.attr('src', 'href');
+    if (!path.parse(attr).hostname) {
+      log('add path for download', attr);
+      pathToRes.push(attr);
+      const name = namingRes(attr);
+      log('change attr %o on %o', attr, name);
+      element.attr('src', 'href', name);
+    }
+  });
+  return [jquery.html(), ...pathToRes];
+};
+
 export default (addr, pathDir) => {
   log('start program, arguments: %o %o', addr, pathDir);
   if (!addr || !pathDir) {
@@ -25,8 +43,7 @@ export default (addr, pathDir) => {
   const filename = namingFile(addr);
   const pathForSave = path.resolve(pathDir, filename);
   const dirForRes = path.resolve(pathDir, `${filename}_files`);
-  const resources = [];
-  let jquery;
+  const answerForEnd = [];
   return axios.get(addr)
     .then(({ data, status }) => {
       if (status !== 200) {
@@ -34,36 +51,29 @@ export default (addr, pathDir) => {
         throw new Error(`statuscode ${status}`);
       }
       log('downlad html');
-      jquery = cheerio.load(data);
-      return Promise.all(['img', 'link', 'script'].map(e => jquery(e).each((i, element) => {
-        const { hostname } = jquery(element).attr('src') ? url.parse(jquery(element).attr('src')) : { hostname: 'not' };
-        if (!hostname) {
-          const name = jquery(element).attr('src').replace(/\//g, (str, offset, s) =>
-            (offset === (s.length - 1 || 0) ? '' : '-'));
-          const config = {
-            url: url.resolve(addr, jquery(element).attr('src')),
-            method: 'get',
-            responseType: 'stream',
-          };
-          log('make config');
-          axios(config).then((img) => {
-            if (img.statusCode !== 200) {
-              throw new Error('can\'t download resources');
-            }
-            const nameForRes = path.resolve(dirForRes, name);
-            resources.push(nameForRes);
-            if (!fs.existsSync(dirForRes)) {
-              fs.mkdir(dirForRes);
-            }
-            fs.writeFile(nameForRes);
-            jquery(element).attr('src', nameForRes);
-            img.data.pipe(fs.createWriteStream(nameForRes));
-            log('save img', nameForRes);
-          });
-        }
-      })));
-    }).then(() => {
+      return changeHtmlAndDownloadLocalRes(data);
+    }).then(([html, ...resources]) => {
       log('write html');
-      return fs.writeFile(`${pathForSave}.html`, jquery.html());
-    }).then(() => [`${pathForSave}.html`, ...resources]);
+      const promises = [];
+      answerForEnd.push(`${pathForSave}.html`);
+      promises.push(fs.writeFile(answerForEnd[0], html));
+      resources.forEach((resource) => {
+        const name = namingRes(resource);
+        promises.push(axios({ url: url.resolve(addr, resource), responseType: 'stream' }).then((img) => {
+          if (img.statusCode !== 200) {
+            throw new Error('can\'t download resources');
+          }
+          const nameForRes = path.resolve(dirForRes, name);
+          answerForEnd.push(nameForRes);
+          if (!fs.existsSync(dirForRes)) {
+            log('make dir for resource', dirForRes);
+            fs.mkdir(dirForRes);
+          }
+          fs.writeFile(nameForRes);
+          img.data.pipe(fs.createWriteStream(nameForRes));
+          log('save img', nameForRes);
+        }));
+      });
+      return Promise.all(promises);
+    }).then(() => answerForEnd);
 };
