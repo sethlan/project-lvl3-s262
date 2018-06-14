8e515dab2d985a9cbb53977e87b30d0e09ace416
sethlans <sethlans@protonmail.com>
2018-06-14 17:09:49 +0800

    new method for errors test

--- __tests__/test.pageloader.js
+++ __tests__/test.pageloader.js
@@ -3,70 +3,49 @@ import nock from 'nock';
 import fs from 'mz/fs';
 import path from 'path';
 import debug from 'debug';
-import pageload from '../src/';
+import cheerio from 'cheerio';
+import loadpage from '../src/';
 
 debug.enable('page-loader');
 const dataForSave = 'test';
 const host = 'http://www.example.com';
 const pathName = '/';
 const textError = 'something awful happened';
-const err = new Error(textError);
-const errStatus = new Error('Request failed with status code 400');
-const folderForTest = path.join(os.tmpdir(), 'pageloader');
+const folderForTest = path.join(os.tmpdir(), 'loadpageer');
 
 test('test for hyper text only', async () => {
   nock(host).get(pathName).reply(200, dataForSave);
   const folder = await fs.mkdtemp(folderForTest);
-  const result = await pageload(host + pathName, folder);
-  const dataFromFunc = await fs.readFile(result[0], 'utf8');
+  const pathToResult = await loadpage(`${host}${pathName}`, folder);
+  const dataFromFunc = await fs.readFile(pathToResult, 'utf8');
   return expect(dataFromFunc).toBe(dataForSave);
+});
+
+test('test for hyper text and image', async () => {
+  const html = await fs.readFile('__tests__/__fixtures__/test.html');
+  const img1 = await fs.readFile('__tests__/__fixtures__/test.png', 'binary');
+  nock(host).get(pathName).reply(200, html);
+  nock(host).get('/test.png').reply(200, img1);
+  const folder = await fs.mkdtemp(folderForTest);
+  const pathsToFiles = await loadpage(`${host}${pathName}`, folder);
+  const jquery = cheerio.load(html);
+  jquery('img').attr('src', path.resolve(folder, 'www-example-com_files/test.png'));
+  const newHtml = jquery.html();
+  const resHtml = await fs.readFile(pathsToFiles[0], 'utf8');
+  const resImg1 = await fs.readFile(pathsToFiles[1], 'binary');
+  return expect(resHtml).toBe(newHtml).expect(resImg1).toBe(img1);
 });
 
 test('test error handle', async () => {
   nock(host).get(pathName).replyWithError(textError);
   const folder = await fs.mkdtemp(folderForTest);
-  return pageload(host + pathName, folder)
-    .catch(e => expect(e).toEqual(err));
+  return expect(loadpage(`${host}${pathName}`, folder))
+    .toThrowErrorMatchingSnapshot();
 });
 
-test('test error statuscode', async () => {
+test('test error statuscode 400', async () => {
   nock(host).get(pathName).reply(400);
   const folder = await fs.mkdtemp(folderForTest);
-  return pageload(host + pathName, folder)
-    .catch(e => expect(e).toEqual(errStatus));
+  return expect(loadpage(`${host}${pathName}`, folder))
+    .toThrowErrorMatchingSnapshot();
 });
-
-// test('test for hyper text and image', async () => {
-//   const html = await fs.readFile('__tests__/__fixtures__/test.html');
-//   const img1 = await fs.readFile('__tests__/__fixtures__/test.png', 'binary');
-//   nock(host)
-//     .get(pathName)
-//     .reply(200, html)
-//     .get('/test.png')
-//     .reply(200, img1);
-//   const folder = await fs.mkdtemp(folderForTest);
-//   const pathsToFiles = await pageload(host + pathName, folder);
-//   const resHtml = await fs.readFile(pathsToFiles[0], 'utf8');
-//   const resImg1 = await fs.readFile(pathsToFiles[1], 'binary');
-//   return expect(resImg1).toBe(img1)
-//     .expect(html).toBe(resHtml);
-// });
-
-// test('test for hyper text and two images', async () => {
-//   const html = await fs.readFile('__tests__/__fixtures__/test.html');
-//   const img1 = await fs.readFile('__tests__/__fixtures__/test.png', 'binary');
-//   const img2 = await fs.readFile('__tests__/__fixtures__/test2.png', 'binary');
-//   nock(host)
-//     .get(pathName)
-//     .reply(200, html)
-//     .get('/test.png')
-//     .reply(200, img1)
-//     .get('/test2.png')
-//     .reply(200, img2);
-//   const folder = await fs.mkdtemp(folderForTest);
-//   const pathsToFiles = await pageload(host + pathName, folder);
-//   const resHtml = await fs.readFile(pathsToFiles[0], 'utf8');
-//   const resImg1 = await fs.readFile(pathsToFiles[1], 'binary');
-//   const resImg2 = await fs.readFile(pathsToFiles[2], 'binary');
-//   return expect([resHtml, resImg1, resImg2]).toBe([html, img1, img2]);
-// });
